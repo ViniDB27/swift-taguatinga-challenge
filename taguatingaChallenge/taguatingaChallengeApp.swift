0751d074ed087a64e8e6ef6af1d134a958f08a98
Vinicio Brejinski <viniciosdb@gmail.com>
2023-02-24 15:37:51 -0300

    Initial Commit

---
+++ taguatingaChallenge/taguatingaChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  taguatingaChallengeApp.swift
+//  taguatingaChallenge
+//
+//  Created by Vinicio Brejinski on 24/02/23.
+//
+
+import SwiftUI
+
+@main
+struct taguatingaChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
